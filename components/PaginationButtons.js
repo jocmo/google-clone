a767dd3e94fb5b251dafc47a8dd0e667ab261c9c
jocmo <joselito.tocmo@gmail.com>
2021-07-02 22:27:24 +0800

    bug fix

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -8,14 +8,14 @@ function PaginationButtons() {
     return (
         <div className="flex justify-between max-w-lg text-blue-700 mb-10">
             {startIndex >=10 && (
-                <Link href={`/search?term=${router.query.term}&start=${startIndex - 10 }`}>
+                <Link href={`/search?term=${router.query.term}&start=${startIndex - 10 }`} passHref={true}>
                     <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
                         <ChevronLeftIcon className="h-5"/>
                         <p>Previous</p>
                     </div>
                 </Link>
             )}
-            <Link href={`/search?term=${router.query.term}&start=${startIndex + 10 }`}>
+            <Link href={`/search?term=${router.query.term}&start=${startIndex + 10 }`} passHref={true}>
                 <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
                     <ChevronRightIcon className="h-5 cursor-pointer"/>
                     <p>Next</p>
