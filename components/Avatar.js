07afb35dca852e58acbeb21d8e2d9bb28c91ba8b
Joselito Tocmo <joselito.tocmo@gmail.com>
2021-07-02 15:41:28 +0800

    bug fix

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,9 +1,12 @@
+// import Image from 'next/image'
+
 
 function Avatar({ url, className }) {
     return <img loading="lazy"
         src={url}
         alt="profile pic"
-        className={`rounded-full h-10 cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}/>
+        className={`rounded-full h-10 cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
+        />
     
 }
 
